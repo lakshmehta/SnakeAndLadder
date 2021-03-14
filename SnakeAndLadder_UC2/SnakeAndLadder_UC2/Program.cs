db9fc6a12b0aa73b09725131c6b810d497168a34
unknown <lakshmehta06@gmail.com>
2021-03-15 01:25:31 +0530

    [LAKSH ADDED]

---
+++ SnakeAndLadder_UC2/SnakeAndLadder_UC2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeAndLadder_UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int player1 = 0;
+            Random random = new Random();
+            Console.WriteLine(random.Next(1, 6));
+      
+
+        }
+    }
+}
