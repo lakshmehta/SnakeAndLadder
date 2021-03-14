749793cf7cc40115ecaa9515ddfe8f05ee656e96
unknown <lakshmehta06@gmail.com>
2021-03-15 01:16:26 +0530

    [LAKSH ADDED]

---
+++ SnakeAndLadder_UC1/SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int player = 0;
+            Console.WriteLine("Player at starting position : " + player);
+        }
+    }
+}
